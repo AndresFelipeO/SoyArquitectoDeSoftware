9181c9bf1fccfa919f68819070b05df5caac65aa
Carsolarte <carsolarte@unicauca.edu.co>
2022-11-22 08:52:36 -0500

    Se quemo comentarios para cada capsula

--- lib/model/Datos.dart
+++ lib/model/Datos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_lorem/flutter_lorem.dart';
+import 'package:soy_arquitecto/controller/Comentario.dart';
 import 'package:soy_arquitecto/controller/EnsayoCritico.dart';
 
 import '../controller/UsuarioSuscrito.dart';
@@ -6,7 +7,7 @@ import '../controller/UsuarioSuscrito.dart';
 class Datos {
   List<UsuarioSuscrito> usuariosPrueba = [
     UsuarioSuscrito("Daniela"),
-    UsuarioSuscrito("Ano"),
+    UsuarioSuscrito("Carito"),
     UsuarioSuscrito("Santi"),
   ];
   Datos() {
@@ -18,6 +19,9 @@ class Datos {
       EnsayoCritico ensayo = EnsayoCritico(
           lorem(paragraphs: 1, words: 2), lorem(paragraphs: 4, words: 80));
       ensayo.publicar(usuariosPrueba[i]);
+      Comentario miCometario =
+          Comentario(usuariosPrueba[1].nombre, lorem(paragraphs: 1, words: 2));
+      ensayo.agregarComentario(miCometario);
     }
   }
 }
